35f6899046012a5635ce7edde2a4b166ad38107b
Khanh Nguyen Viet <khanhnv@nttdata.com.vn>
2018-01-29 10:47:37 +0700

    i18n

--- src/app/banner-inline/banner-inline.component.ts
+++ src/app/banner-inline/banner-inline.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BannerInlineComponent implements OnInit {
 
   title = "Banner";
+  htmlSnippet = 'Template <script>alert("abc")</script> <b>Syntax</b>';
 
   constructor() { }
 
